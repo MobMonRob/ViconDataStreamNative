1dc38df82869862cc50c23f273f1ec3d47e4880c
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-06-16 13:12:40 +0200

    removed dependency on specific mingw version

--- _project_config.sh
+++ _project_config.sh
@@ -4,5 +4,7 @@
 readonly projectDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 readonly boostDir="$projectDir/_dependencies/CustomBoostBuild"
 readonly mingwLibPath1="/usr/x86_64-w64-mingw32/lib"
-readonly mingwLibPath2="/usr/lib/gcc/x86_64-w64-mingw32/7.3-posix"
 
+local -r minwLibPath2Beginning="/usr/lib/gcc/x86_64-w64-mingw32"
+local -r minwLibPath2FoundArray=($(find "$minwLibPath2Beginning"/*-posix -maxdepth 0 -mindepth 0 -type d -printf '%f\n'))
+readonly mingwLibPath2="$minwLibPath2Beginning/${minwLibPath2FoundArray[0]}"
