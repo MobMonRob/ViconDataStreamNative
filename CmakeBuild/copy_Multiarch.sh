6738b73fa0c13b1bce27538d620e37888bfe9af6
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-10-13 20:22:35 +0200

    removed copying of StringFactory.h

--- CmakeBuild/copy_Multiarch.sh
+++ CmakeBuild/copy_Multiarch.sh
@@ -17,7 +17,6 @@ run() {
 	cp -L -l "$tmpDir"/lib* "$targetDir"
 
 	cp -L -l "../current_Vicon_source/Vicon/CrossMarket/DataStream/ViconDataStreamSDK_CPP"/*DataStream*Client*.h "$targetDir"
-	cp -L -l "../current_Vicon_source/Vicon/CrossMarket/DataStream/ViconDataStreamSDK_CPP"/StringFactory.h "$targetDir"
 
 	cp -L -l "../ViconStringTest.h" "$targetDir"
 }
